7f4057671fb2033ab4f160b5cc328847ef1ec4c9
dardorsh <65073890+dardorsh@users.noreply.github.com>
2025-09-22 20:35:23 +0300

    add fix after review

--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -1,16 +1,20 @@
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 
 
 public class TestBase {
 
-    @BeforeEach
-    void setup() {
+
+    @BeforeAll
+    void setupBeforeAll() {
         Configuration.browser = "chrome";
         Configuration.pageLoadStrategy = "normal";
-
+    }
+    @BeforeEach
+    void setupBeforeEach() {
         SelenideLogger.addListener("allure", new AllureSelenide());
     }
 }
